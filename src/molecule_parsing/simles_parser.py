1e366ebd8b83d1f93770df73654b3d68c2e9b591
Florian Bauernfeind <e11775814@student.tuwien.ac.at>
2022-06-21 11:01:07 +0200

    add parser class

---
+++ src/molecule_parsing/simles_parser.py
@@ -0,0 +1,20 @@
+
+
+# https://github.com/pckroon/pysmiles
+# https://neo4j.com/developer/python/
+
+from typing import List
+
+class SmilesParser:
+
+    def __init__(self):
+        pass
+
+    def parse_smiles_molecule(self, smiles: str):
+        pass
+
+    def parse_smiles_list(self, smiles_list: List[str]):
+        pass
+
+    def store_smiles_list_in_neo4j(self, smiles_list: List[str], neo4j_con):
+        pass
